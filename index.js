fb4deefeda5d380639ea9a8915046aece8c41862
strelok93 <strelok0493@gmail.coom>
2024-04-27 00:54:29 +0500

    backend

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const jsonServer = require('json-server')
-const middleware = require('./middleware')
-const server = jsonServer.create()
+const jsonServer = require("json-server");
+const middleware = require("./middleware");
+const server = jsonServer.create();
 
-const router = jsonServer.router('./db.json')
+const router = jsonServer.router("./db.json");
 
-const middlewares = jsonServer.defaults()
-server.use(middleware)
-server.use(middlewares)
-server.use(router)
-const port = 10000
-server.listen(port, '0.0.0.0', () => {
-  console.log(`Json Server is running on port ${port}`)
-})
+server.get("/products-count", (req, res, next) => {
+  const data = router.db.getState().products.length;
+  res.json(data);
+});
+
+const middlewares = jsonServer.defaults();
+server.use(middleware);
+server.use(middlewares);
+server.use(router);
+const port = 10000;
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Json Server is running on port ${port}`);
+});
